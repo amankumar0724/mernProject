792c2aca2c85b281bed86a01ad0821a047680a65
amankumar0724 <ask5081811@gmail.com>
2025-02-25 07:14:44 +0530

    bug fix

--- server/server.mjs
+++ server/server.mjs
@@ -17,6 +17,7 @@ const corsOptions = {
     origin: "https://thinksync-think-write-sync.onrender.com/",
     credentials: true,
 }
+
 app.use(cors(corsOptions));
 app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}));
